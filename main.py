8d7056539db1836090a3f99bfd6a81ac0d0d7cb5
fadifadifadifadifadi157-glitch <fadifadifadifadifadi157@gmail.com>
2026-08-11 23:52:17 +0500

    Handle invalid input

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-number = int(input("Enter a number: "))
+try:
+    number = int(input("Enter a number: "))
 
-if number %2== 0:
-     print("The number is even!")
-else:
-     print("The number is odd!")
+    if number % 2 == 0:
+        print("The number is even!")
+    else:
+        print("The number is odd!")
+
+except ValueError:
+    print("Invalid input! Please enter a whole number.")
