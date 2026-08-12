98619f20edf6bef7f55e364b64cad2f4860666fb
fadifadifadifadifadi157-glitch <fadifadifadifadifadi157@gmail.com>
2026-08-12 23:14:28 +0500

    Add loop for repeated number checks

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-try:
-    number = int(input("Enter a number: "))
+while True:
+    try:
+        number = int(input("Enter a number: "))
 
-    if number % 2 == 0:
-        print("The number is even!")
-    else:
-        print("The number is odd!")
+        if number %2== 0:
+            print("The number is even!")
+        else:
+            print("The number is odd!")
 
-except ValueError:
-    print("Invalid input! Please enter a whole number.")
+    except ValueError:
+        print("Invalid input! Please enter a whole number.")
+
+    again =input("Do you want to check another number? (yes/no): ")
+
+    if again.lower() !="yes":
+        print("Goodbye!")
+        break
